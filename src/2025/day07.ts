ead40c48b81dc7b988d2ece9e275b827ec93e6c8
Luis Escobar Sawa <6599003+LeJawa@users.noreply.github.com>
2025-12-07 10:57:33 +0100

    Day 07 2025 part 1

---
+++ src/2025/day07.ts
@@ -0,0 +1,55 @@
+import { readInput, timeIt } from "../common";
+import { type Solution } from "../types";
+
+const year = 2025;
+const day = 7;
+const lines = readInput(day, year);
+
+type DayInputType = string[];
+
+// Returns the manipulated input lines
+function setup(lines: string[]): DayInputType {
+  return lines.filter((line) => /[\^S]/.test(line));
+}
+
+function part1(input: DayInputType): Solution {
+  const start = input[0].indexOf("S");
+  let beams = new Set([start]);
+
+  let splits = 0;
+
+  for (let i = 1; i < input.length; i++) {
+    const newBeams: Set<number> = new Set();
+
+    beams.forEach((beam) => {
+      if (input[i][beam] === "^") {
+        splits++;
+        newBeams.add(beam - 1);
+        newBeams.add(beam + 1);
+      } else newBeams.add(beam);
+    });
+    beams = newBeams;
+  }
+
+  return splits;
+}
+
+function part2(input: DayInputType): Solution {
+  // TODO: Implement Part 2
+  return null;
+}
+
+export async function main() {
+  console.log(`Day ${day}:`);
+
+  const { result: dayInput, time: time0 } = await timeIt(() => setup(lines));
+  console.log(` > Setup (${time0})`);
+
+  const { result: answer1, time: time1 } = await timeIt(() => part1(dayInput));
+  console.log(` > Part 1:`, answer1, `(${time1})`);
+
+  const { result: answer2, time: time2 } = await timeIt(() => part2(dayInput));
+  console.log(` > Part 2:`, answer2, `(${time2})`);
+}
+
+export { part1, part2 };
