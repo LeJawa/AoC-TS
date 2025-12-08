9325754ac547c82863c2c4cd89bb348040a46d8c
Luis Escobar Sawa <luis.escobar@mailbox.org>
2025-12-08 15:46:58 +0100

    Day 08 2025 First try

---
+++ src/2025/day08.ts
@@ -0,0 +1,147 @@
+import { readInput, timeIt } from "../common";
+import { type Solution } from "../types";
+
+const year = 2025;
+const day = 8;
+
+const IS_RAW_INPUT = false; // set to true if lines is raw input
+const lines = readInput(day, year, IS_RAW_INPUT);
+
+type DayInputType = {
+  [key: string]: number[];
+};
+
+// Returns the manipulated input lines
+function setup(lines: string[]) {
+  const positions = lines.map((line) =>
+    line.split(",").map((x) => parseInt(x))
+  );
+
+  const boxes: { [key: string]: number[] } = {};
+  positions.forEach((position) => {
+    boxes[position.toString()] = position;
+  });
+
+  return boxes;
+}
+
+const getDistance = (a: number[], b: number[]) => {
+  return Math.sqrt(
+    (b[0] - a[0]) * (b[0] - a[0]) +
+      (b[1] - a[1]) * (b[1] - a[1]) +
+      (b[2] - a[2]) * (b[2] - a[2])
+  );
+};
+
+const findSmallestDistance = (distances: {
+  [fromBox: string]: { [toBox: string]: number };
+}) => {
+  let minDist = Infinity;
+  let from = "",
+    to = "";
+
+  for (const f in distances) {
+    for (const t in distances[f]) {
+      if (!!distances[f][t] && distances[f][t] < minDist) {
+        minDist = distances[f][t];
+        from = f;
+        to = t;
+      }
+    }
+  }
+
+  return [from, to];
+};
+
+function part1(boxes: DayInputType): Solution {
+  const distances: { [fromBox: string]: { [toBox: string]: number } } = {};
+
+  for (const from in boxes) {
+    if (distances[from] === undefined) distances[from] = {};
+    for (const to in boxes) {
+      if (from === to) continue;
+      if (distances[to] && distances[to][from] !== undefined) continue;
+
+      distances[from][to] = getDistance(boxes[from], boxes[to]);
+    }
+  }
+  const graphs: Set<string>[] = [];
+
+  for (let _ = 0; _ < 1000; _++) {
+    // console.log(_);
+
+    const [from, to] = findSmallestDistance(distances);
+    distances[from][to] = Infinity;
+    distances[to][from] = Infinity;
+
+    let addedToGraph = false;
+    let toDelete = -1;
+    for (const graph of graphs) {
+      if (graph.has(from)) {
+        for (let i = 0; i < graphs.length; i++) {
+          if (graphs[i] === graph) continue;
+          if (graphs[i].has(to)) {
+            for (const f of graphs[i].values()) {
+              graph.add(f);
+            }
+            toDelete = i;
+            break;
+          }
+        }
+
+        graph.add(to);
+        addedToGraph = true;
+        break;
+      }
+      if (graph.has(to)) {
+        for (let i = 0; i < graphs.length; i++) {
+          if (graphs[i] === graph) continue;
+          if (graphs[i].has(from)) {
+            for (const t of graphs[i].values()) {
+              graph.add(t);
+            }
+            toDelete = i;
+            break;
+          }
+        }
+
+        graph.add(from);
+        addedToGraph = true;
+        break;
+      }
+    }
+    if (!addedToGraph) {
+      graphs.push(new Set([from, to]));
+    } else if (toDelete !== -1) {
+      graphs.splice(toDelete, 1);
+    }
+
+    // console.log(`${from} => ${to}`);
+
+    // console.log(graphs);
+  }
+
+  const sorted = graphs.toSorted((a, b) => b.size - a.size);
+  console.log(sorted.slice(0, 10));
+  return sorted[0].size * sorted[1].size * sorted[3].size;
+}
+
+function part2(boxes: DayInputType): Solution {
+  // TODO: Implement Part 2
+  return null;
+}
+
+export async function main() {
+  console.log(`Day ${day}:`);
+
+  const { result: dayInput, time: time0 } = await timeIt(() => setup(lines));
+  console.log(` > Setup (${time0})`);
+
+  const { result: answer1, time: time1 } = await timeIt(() => part1(dayInput));
+  console.log(` > Part 1:`, answer1, `(${time1})`);
+
+  const { result: answer2, time: time2 } = await timeIt(() => part2(dayInput));
+  console.log(` > Part 2:`, answer2, `(${time2})`);
+}
+
+export { setup, part1, part2, IS_RAW_INPUT };
