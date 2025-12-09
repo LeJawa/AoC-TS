dd36507411e9f143b1430740b040a949f51edd54
Luis Escobar Sawa <luis.escobar@mailbox.org>
2025-12-09 10:13:43 +0100

    Day 08 2025 part 1 => 2-3x time improvement

--- src/2025/day08.ts
+++ src/2025/day08.ts
@@ -26,10 +26,10 @@ function setup(lines: string[]) {
 }
 
 const getDistance = (a: number[], b: number[]) => {
-  return Math.sqrt(
+  return (
     (b[0] - a[0]) * (b[0] - a[0]) +
-      (b[1] - a[1]) * (b[1] - a[1]) +
-      (b[2] - a[2]) * (b[2] - a[2])
+    (b[1] - a[1]) * (b[1] - a[1]) +
+    (b[2] - a[2]) * (b[2] - a[2])
   );
 };
 
@@ -53,9 +53,43 @@ const findSmallestDistance = (distances: {
   return [from, to];
 };
 
+type Distance = {
+  nodeA: string;
+  nodeB: string;
+  value: number;
+};
+
+const addToShortestDistances = (
+  distance: Distance,
+  shortestDistances: Distance[],
+  N: number
+) => {
+  if (shortestDistances.length < N) {
+    shortestDistances.push(distance);
+    shortestDistances.sort((a, b) => a.value - b.value);
+    return shortestDistances;
+  }
+
+  const index = shortestDistances.findIndex((d, i, arr) => {
+    return (
+      distance.value > d.value &&
+      distance.value < (arr[i + 1] ?? { value: Infinity }).value
+    );
+  });
+
+  const unsorted = shortestDistances
+    .slice(0, index + 1)
+    .concat(distance)
+    .concat(shortestDistances.slice(index + 1, N - 1));
+
+  return unsorted.toSorted((a, b) => a.value - b.value);
+};
+
 function part1(boxes: DayInputType): Solution {
-  return 131580;
+  // return 131580;
+  const N = 1000;
   const distances: { [fromBox: string]: { [toBox: string]: number } } = {};
+  let shortestDistances: Distance[] = [];
 
   for (const from in boxes) {
     if (distances[from] === undefined) distances[from] = {};
@@ -64,24 +98,32 @@ function part1(boxes: DayInputType): Solution {
       if (distances[to] && distances[to][from] !== undefined) continue;
 
       distances[from][to] = getDistance(boxes[from], boxes[to]);
+
+      shortestDistances = addToShortestDistances(
+        {
+          nodeA: from,
+          nodeB: to,
+          value: distances[from][to],
+        },
+        shortestDistances,
+        N
+      );
     }
   }
   const graphs: Set<string>[] = [];
 
-  for (let _ = 0; _ < 1000; _++) {
-    // console.log(_);
+  for (let j = 0; j < N; j++) {
+    console.log(j);
 
-    const [from, to] = findSmallestDistance(distances);
-    distances[from][to] = Infinity;
-    distances[to][from] = Infinity;
+    const { nodeA, nodeB } = shortestDistances[j];
 
     let addedToGraph = false;
     let toDelete = -1;
     for (const graph of graphs) {
-      if (graph.has(from)) {
+      if (graph.has(nodeA)) {
         for (let i = 0; i < graphs.length; i++) {
           if (graphs[i] === graph) continue;
-          if (graphs[i].has(to)) {
+          if (graphs[i].has(nodeB)) {
             for (const f of graphs[i].values()) {
               graph.add(f);
             }
@@ -90,14 +132,14 @@ function part1(boxes: DayInputType): Solution {
           }
         }
 
-        graph.add(to);
+        graph.add(nodeB);
         addedToGraph = true;
         break;
       }
-      if (graph.has(to)) {
+      if (graph.has(nodeB)) {
         for (let i = 0; i < graphs.length; i++) {
           if (graphs[i] === graph) continue;
-          if (graphs[i].has(from)) {
+          if (graphs[i].has(nodeA)) {
             for (const t of graphs[i].values()) {
               graph.add(t);
             }
@@ -106,13 +148,13 @@ function part1(boxes: DayInputType): Solution {
           }
         }
 
-        graph.add(from);
+        graph.add(nodeA);
         addedToGraph = true;
         break;
       }
     }
     if (!addedToGraph) {
-      graphs.push(new Set([from, to]));
+      graphs.push(new Set([nodeA, nodeB]));
     } else if (toDelete !== -1) {
       graphs.splice(toDelete, 1);
     }
@@ -123,7 +165,7 @@ function part1(boxes: DayInputType): Solution {
   }
 
   const sorted = graphs.toSorted((a, b) => b.size - a.size);
-  console.log(sorted.slice(0, 10));
+  console.log(sorted.slice(0, 3));
   return sorted[0].size * sorted[1].size * sorted[2].size;
 }
 
@@ -208,11 +250,11 @@ export async function main() {
   const { result: dayInput, time: time0 } = await timeIt(() => setup(lines));
   console.log(` > Setup (${time0})`);
 
-  // const { result: answer1, time: time1 } = await timeIt(() => part1(dayInput));
-  // console.log(` > Part 1:`, answer1, `(${time1})`);
+  const { result: answer1, time: time1 } = await timeIt(() => part1(dayInput));
+  console.log(` > Part 1:`, answer1, `(${time1})`);
 
-  const { result: answer2, time: time2 } = await timeIt(() => part2(dayInput));
-  console.log(` > Part 2:`, answer2, `(${time2})`);
+  // const { result: answer2, time: time2 } = await timeIt(() => part2(dayInput));
+  // console.log(` > Part 2:`, answer2, `(${time2})`);
 }
 
 export { setup, part1, part2, IS_RAW_INPUT };
